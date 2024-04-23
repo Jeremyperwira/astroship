5c9476ebfeb9dfb22332ba68f8fde0e1919e4758
jeremyperwira <jeremyperwira@protonmail.com>
2024-04-24 00:05:52 +0700

    *

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,25 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        text: 'text 5s ease infinite',   
+      },
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
-    },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    }
   },
   plugins: [require("@tailwindcss/typography")],
 };
